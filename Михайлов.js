7aab5ff53eb18e0a2c51d766ddcf2a0fa7097658
a-mihaylov <a.s.mihaylov@yandex.ru>
2019-03-06 12:30:39 +0400

    Second commit to master. Удалены импорты.

--- Михайлов.js
+++ Михайлов.js
@@ -1,13 +1,3 @@
-import React, { Component } from "react";
-import Container from 'react-bootstrap/lib/Container'
-import Col from 'react-bootstrap/lib/Col'
-import Row from 'react-bootstrap/lib/Row'
-import Form from 'react-bootstrap/lib/Form'
-import Button from 'react-bootstrap/lib/Button'
-
-import { GoogleButton } from "./social-buttons/GoogleButton";
-import { VkButton } from "./social-buttons/VkButton";
-
 export class LoginOrSignup extends Component {
     constructor(props) {
         super(props);
