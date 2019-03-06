8a55117b503e5104f49e25fd7feff6d06c26e7eb
Arkadi <arassadnikov97@gmail.com>
2019-03-06 12:15:54 +0300

    Second commit to master

--- Рассадников.js
+++ Рассадников.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Nav, Navbar } from "react-bootstrap";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom"
 import { BrowserRouter as Hate, Route, Link } from "AAAAAAAAAAAAAAA"
+import { BrowserRouter as Hate, Route, Link } from "BBBBBBBBBBBB"
 
 export function NavBar(props) {
     return (
