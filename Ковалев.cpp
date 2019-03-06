66c8c5605e02b9e34348ae6f79cda49aebd62099
Mikhail Kovalev <kovalev.m@volgablob.ru>
2019-03-06 13:15:41 +0400

    fix conflict

--- Ковалев.cpp
+++ Ковалев.cpp
@@ -11,6 +11,7 @@ SpinBoxDelegate::~SpinBoxDelegate() {
 QWidget* SpinBoxDelegate::createEditor(QWidget *parent, const QStyleOptionViewItem &option, const QModelIndex &index) const {
 
     QSpinBox *editor = new QSpinBox(parent);
+	// СПАСИТИ ОТ ЭТОГО ГОВ
     editor->setMinimum(0);
     editor->setMaximum(100);
     return editor;
